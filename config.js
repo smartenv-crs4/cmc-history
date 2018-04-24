7990950ba4acee2344369eaa86b91e2662873f4a
Alberto <alberto.buschettu@italybureau.net>
2018-04-24 17:06:13 +0200

    Fix require path in config

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 var config = require('./config/default.json');
 var async=require('async');
 var argv = require('minimist')(process.argv.slice(2));
-var version = require('package.json').version;
+var version = require('./package.json').version;
 console.dir(argv);
 
 var conf;
